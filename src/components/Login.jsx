966c5d06d3874bcef747c561492a8bddcf41fb9c
Baran Sülü <73203587+Barannsulu@users.noreply.github.com>
2022-03-16 11:49:58 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <Container>
