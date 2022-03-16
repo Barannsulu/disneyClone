eb4b7df7b97f569637ce331b4cf35592ee7cda3f
Baran Sülü <73203587+Barannsulu@users.noreply.github.com>
2022-03-16 11:45:33 +0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,166 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { auth, provider } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setLogin,
+  setLogout,
+} from "../features/user/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+
+const Header = () => {
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setLogin(user);
+        navigate.push("/home");
+      }
+    });
+  }, [userName]);
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      let user = result.user;
+      dispatch(
+        setLogin({
+          name: user.displayName,
+          email: user.email,
+          photo: user.photoURL,
+        })
+      );
+      navigate("/home");
+    });
+  };
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(setLogout());
+      navigate("/");
+    });
+  };
+
+  return (
+    <Nav>
+      <Link to="/home">
+        <Logo src="/images/logo.svg" />
+      </Link>
+      <NavMenu>
+        <Link to="/home">
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </Link>
+
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <Created>Created by @barannsulu</Created>
+      <UserImg
+        onClick={signOut}
+        src="https://pbs.twimg.com/profile_images/1406768364693987330/hpGf9Ove_400x400.jpg"
+      />
+    </Nav>
+  );
+};
+
+export default Header;
+
+const Nav = styled.nav`
+  background-color: #090b13;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      width: 20px;
+    }
+
+    span {
+      
+      letter-spacing: 1.4px;
+      font-size: 13px;
+      margin-left: 5px;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background-color: #fff;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -5px;
+        opacity: 0;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(.25 , .46, .45 , .94) 0s;
+      }
+    }
+
+    &:hover {
+        span:after{
+            transform: scaleX(1);
+            opacity: 1;
+        }
+  }
+
+   @media (max-width: 900px) {
+    display: none;
+  } 
+`;
+
+const Created = styled.p`
+  margin-right: 10px;
+  font-size: 12px;
+`;
+
+const UserImg = styled.img`
+  width: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
---
+++ Login.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+const Login = () => {
+  return (
+    <Container>
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg"></CTALogoOne>
+        <Link to="/home">
+          <SignUp>GET ALL THERE</SignUp>
+        </Link>
+        <Description>
+          Get Premier Access to Raya and the Last Dragon for an additional fee
+          with a Disney+ subscription. As of 03/26/21, the price of Disney+ and
+          The Disney Bundle will increase by $1.
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png"></CTALogoTwo>
+      </CTA>
+    </Container>
+  );
+};
+
+export default Login;
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 70px);
+
+  &:before {
+    background-position: top;
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    content: "";
+    background-image: url("/images/login-background.jpg");
+    z-index: -1;
+  }
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+  padding: 80px 40px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CTALogoOne = styled.img``;
+
+const SignUp = styled.div`
+  width: 570px;
+  background-color: #0063e5;
+  font-weight: 600;
+  padding: 17px 0;
+  margin-top: 10px;
+  border-radius: 5px;
+  text-align: center;
+  font-size: 18px;
+  cursor: pointer;
+  letter-spacing: 1.5px;
+  margin-bottom: 12px;
+
+  &:hover {
+    background: #0483ee;
+  }
+
+  @media (max-width: 768px) {
+    width: 400px;
+  }
+
+  @media (max-width: 768px) {
+    width: 340px;
+  }
+`;
+
+const Description = styled.p`
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+  font-size: 11px;
+  text-align: center;
+`;
+
+const CTALogoTwo = styled.img`
+  width: 90%;
+`;
